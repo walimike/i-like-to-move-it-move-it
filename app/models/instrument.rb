414d277ff2814b29bd3678335cb3d96efd5d00f4
Michael Wali <walimike139@gmail.com>
2019-12-05 11:25:28 +0300

    add move it app

---
+++ app/models/instrument.rb
@@ -0,0 +1,24 @@
+class Instrument < ApplicationRecord
+  before_destroy :not_referenced_by_any_line_item
+  belongs_to :user, optional: true
+  has_many :line_items
+
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON # If you use SQLite, add this line
+
+  validates :title, :price, presence: true
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
+
+  CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+  private
+
+  def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
+
+end
